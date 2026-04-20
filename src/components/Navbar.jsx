33e5790d4e2f6eba4c160777f58e53d42df3841b
Vivek Kumar <vivek181004@gmail.com>
2026-04-20 10:13:11 +0530

    removing the warnings in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
 
             <a
               href={`tel:${contactInfo.phone}`}
-              className="hidden lg:flex flex items-center gap-2 bg-blue-500 text-white hover:bg-blue-600 p-2 rounded-full"
+              className="hidden lg:flex items-center gap-2 bg-blue-500 text-white hover:bg-blue-600 p-2 rounded-full"
             >
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
                 <path d="M6.62 10.79a15.054 15.054 0 006.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V21c0 .55-.45 1-1 1C10.07 22 2 13.93 2 4c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.24.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
